deb5b61489fe7c8f06d74af738d6d719de1ed050
Giocatory <nivolyas@gmail.com>
2021-07-02 18:29:20 +0300

    Добавьте файлы проекта.

---
+++ CFRandFNK/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CFRandFNK
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double cfr = Convert.ToDouble(CFR.Text);
+            double fnk = Convert.ToDouble(FNK.Text);
+
+            double calc = (cfr * 0.03 + 10) / fnk;
+
+            Result.Text = $"Вам пришло FNK по цене {calc:f5}";
+        }
+    }
+}
